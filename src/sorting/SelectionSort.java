bc392583b75e19f41799ba758ecf2bb6ecb95b0f
carlos <cruiz@intelenz.com>
2018-07-31 21:57:43 -0700

    POWN-000 Refactored selection sort after study

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,21 +1,27 @@
 package sorting;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * 
- * Selection sort One of the simplest sorting algorithms works as follows: First, find
- * the smallest item in the array and exchange it with the first entry (itself if the first entry
- * is already the smallest). Then, find the next smallest item and exchange it with the second
- * entry. Continue in this way until the entire array is sorted. This method is called
- * selection sort because it works by repeatedly selecting the smallest remaining item.
-*/
+ * Selection sort One of the simplest sorting algorithms works as follows:
+ * First, find the smallest item in the array and exchange it with the first
+ * entry (itself if the first entry is already the smallest). Then, find the
+ * next smallest item and exchange it with the second entry. Continue in this
+ * way until the entire array is sorted. This method is called selection sort
+ * because it works by repeatedly selecting the smallest remaining item.
+ */
 public class SelectionSort extends SortingAlgorithm {
+	private static final Logger LOG = Logger.getLogger(SelectionSort.class.getName());
+
 	public SelectionSort() {
 		super();
 	}
 
 	@Override
 	public int[] runAlgorithm(boolean showtime, int[] a) {
-		System.out.println("SELECTION SORT:");
+		LOG.log(Level.FINE, "Selection sort");
 		if (showtime) {
 			long startTime;
 			long stopTime;
@@ -24,24 +30,23 @@ public class SelectionSort extends SortingAlgorithm {
 			sort(a);
 			stopTime = System.currentTimeMillis();
 			elapsedTime = stopTime - startTime;
-			System.out.println("Elapsed time=" + elapsedTime + "[ms]");
-		} else
+			LOG.log(Level.FINE, "Elapsed time= {0} [ms]", elapsedTime);
+		} else {
 			sort(a);
-
+		}
 		return a;
 	}
 
 	@Override
 	public int[] sort(int a[]) {
-		int indice;
+		int index;
 		for (int i = 0; i < a.length - 1; i++) {
-			indice = i;
+			index = i;
 			for (int j = i + 1; j < a.length; j++) {
-				if (a[indice] > a[j])
-					indice = j;
+				if (a[index] > a[j])
+					index = j;
 			}
-			swap(a, i, indice);// Contabilizara siempre este Intercambio puede suceder
-													// que aveces no suceda nada.
+			swap(a, i, index);
 		}
 		return a;
 	}
