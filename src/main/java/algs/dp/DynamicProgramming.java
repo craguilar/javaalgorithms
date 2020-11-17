e4c2f13e4e472a736ae0134df9a72d0f778c2e32
Carlos Ruiz <caruizag@gmail.com>
2020-11-16 23:17:16 -0800

    [update] - Remove unecessary comment

--- src/main/java/algs/dp/DynamicProgramming.java
+++ src/main/java/algs/dp/DynamicProgramming.java
@@ -4,8 +4,6 @@ package algs.dp;
 public class DynamicProgramming {
 
   /**
-   * Leasson learned : Top Down approach and static caches are likely to cause bug
-   * 
    * You are given coins of different denominations and a total amount of money
    * amount. Write a function to compute the fewest number of coins that you
    * need to make up that amount. If that amount of money cannot be made up by
