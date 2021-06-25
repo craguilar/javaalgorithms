9f83ccc1647ec211bf59ad3b4cb9b6ad46296fc5
Carlos Ruiz <caruizag@gmail.com>
2021-06-24 19:54:27 -0700

    [update] - Added calculation of max land with lake inside

--- src/main/java/algorithms/graph/Islands.java
+++ src/main/java/algorithms/graph/Islands.java
@@ -1,6 +1,10 @@
 package algorithms.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import lombok.Builder;
+import lombok.Data;
 
 /**
  * @caruizag
@@ -14,15 +18,79 @@ public class Islands {
     this.grid = grid;
   }
 
+  /**
+   * Get the max area of an island , land is defined by 1 .
+   * 
+   * @return
+   */
   public int maxAreaOfIsland() {
     int ans = 0;
     boolean[][] seen = new boolean[grid.length][grid[0].length];
     for (int r = 0; r < grid.length; r++) {
       for (int c = 0; c < grid[0].length; c++) {
-        ans = Math.max(ans, areaLand(this.grid, seen, r, c));
+        ans = Math.max(ans, areaLand(seen, r, c));
       }
     }
     return ans;
+  }
+
+  /**
+   * This operation modifies the map, anf fill the lakes with water.
+   */
+  public void fillLakesWithLand() {
+    boolean[][] seen = new boolean[grid.length][grid[0].length];
+    int columnLength = grid[0].length;
+    int rowLength = grid.length;
+    for (int r = 0; r < grid.length; r++) {
+      if (grid[r][0] == 0 && !seen[r][0]) {
+        markBoundaryRegion(r, 0, seen);
+      }
+      if (grid[r][columnLength - 1] == 0 && !seen[r][columnLength - 1]) {
+        markBoundaryRegion(r, columnLength - 1, seen);
+      }
+    }
+
+    for (int c = 0; c < columnLength; c++) {
+      if (grid[0][c] == 0 && !seen[0][c]) {
+        markBoundaryRegion(0, c, seen);
+      }
+      if (grid[rowLength - 1][c] == 0 && !seen[rowLength - 1][c]) {
+        markBoundaryRegion(rowLength - 1, c, seen);
+      }
+    }
+
+    for (int r = 0; r < grid.length; r++) {
+      for (int c = 0; c < columnLength; c++) {
+        if (grid[r][c] == 0 && !seen[r][c]) {
+          grid[r][c] = 1;
+        }
+      }
+    }
+  }
+
+  private void markBoundaryRegion(int r, int c, boolean[][] seen) {
+    Queue<Coordinate> queue = new LinkedList<>();
+    queue.add(Coordinate.builder().x(r).y(c).build());
+    seen[r][c] = true;
+    while (!queue.isEmpty()) {
+      Coordinate curr = queue.poll();
+      final int[][] DIRS = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+      for (int[] dir : DIRS) {
+        Coordinate next = Coordinate.builder().x(curr.getX() + dir[0]).y(curr.getY() + dir[1]).build();
+        if (next.getX() >= 0 && next.getX() < grid.length && next.getY() >= 0 && next.getY() < grid[0].length
+            && grid[next.getX()][next.getY()] == 0 && !seen[next.getX()][next.getY()]) {
+          seen[next.getX()][next.getY()] = true;
+          queue.add(next);
+        }
+      }
+    }
+  }
+
+  @Data
+  @Builder
+  private static class Coordinate {
+    private int x;
+    private int y;
   }
 
   public int countOceanOrLakes() {
@@ -31,7 +99,7 @@ public class Islands {
     for (int r = 0; r < grid.length; r++) {
       for (int c = 0; c < grid[0].length; c++) {
         if (grid[r][c] == 0 && !seen[r][c]) {
-          areaOceansOrLakes(this.grid, seen, r, c);
+          visitWater(seen, r, c);
           pounds++;
         }
       }
@@ -44,7 +112,7 @@ public class Islands {
    */
   public void printMap() {
     for (int r = 0; r < grid.length; r++) {
-      System.out.print("| ");
+      System.out.print(r + ": | ");
       for (int c = 0; c < grid[0].length; c++) {
         System.out.print(grid[r][c] + " ");
       }
@@ -53,21 +121,29 @@ public class Islands {
     }
   }
 
-  private void areaOceansOrLakes(int[][] grid, boolean[][] seen, int r, int c) {
+  /**
+   * This function depends on seen to keep track of visited Coordinates in the
+   * map.
+   * 
+   * @param seen
+   * @param r
+   * @param c
+   */
+  private void visitWater(boolean[][] seen, int r, int c) {
     if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || seen[r][c] || grid[r][c] == 1)
       return;
     seen[r][c] = true;
-    areaOceansOrLakes(grid, seen, r + 1, c);
-    areaOceansOrLakes(grid, seen, r - 1, c);
-    areaOceansOrLakes(grid, seen, r, c - 1);
-    areaOceansOrLakes(grid, seen, r, c + 1);
+    visitWater(seen, r + 1, c);
+    visitWater(seen, r - 1, c);
+    visitWater(seen, r, c - 1);
+    visitWater(seen, r, c + 1);
   }
 
-  private int areaLand(int[][] grid, boolean[][] seen, int r, int c) {
+  private int areaLand(boolean[][] seen, int r, int c) {
     if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || seen[r][c] || grid[r][c] == 0)
       return 0;
     seen[r][c] = true;
-    return (1 + areaLand(grid, seen, r + 1, c) + areaLand(grid, seen, r - 1, c) + areaLand(grid, seen, r, c - 1)
-        + areaLand(grid, seen, r, c + 1));
+    return (1 + areaLand(seen, r + 1, c) + areaLand(seen, r - 1, c) + areaLand(seen, r, c - 1)
+        + areaLand(seen, r, c + 1));
   }
 }
--- src/test/java/algorithms/graph/IslandsTest.java
+++ src/test/java/algorithms/graph/IslandsTest.java
@@ -2,6 +2,9 @@ package algorithms.graph;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class IslandsTest {
@@ -11,14 +14,17 @@ public class IslandsTest {
   private static final int[][] ISLAND_WITH_BAY = new int[][] { { 0, 1, 1, 1 }, { 0, 1, 0, 1 }, { 0, 0, 0, 1 } };
 
   private static final int[][] TWO_ISLANDS_ONE_WITH_LAKE = new int[][] { { 0, 1, 1, 1, 1 }, { 0, 1, 0, 1, 0 },
-      { 0, 1, 0, 1, 0 }, { 0, 1, 1, 1, 0 }, { 0, 0, 0, 1, 0 }, { 1, 0, 0, 0, 0 } };
+      { 0, 1, 1, 1, 0 }, { 0, 0, 0, 1, 0 }, { 1, 0, 0, 0, 0 } };
+
+  private static final int[][] ONE_ISLANDS_ONE_WITH_LAKE = new int[][] { { 0, 1, 0, 1, 1 }, { 0, 1, 0, 1, 0 },
+      { 0, 1, 1, 1, 0 }, { 0, 0, 0, 1, 0 }, { 1, 0, 0, 0, 0 } };
 
   @Test
   public void maxAreaOfIsland() {
 
     assertEquals(8, Islands.builder().grid(ONE_ISLAND_WITH_LAKE).build().maxAreaOfIsland());
     assertEquals(6, Islands.builder().grid(ISLAND_WITH_BAY).build().maxAreaOfIsland());
-    assertEquals(12, Islands.builder().grid(TWO_ISLANDS_ONE_WITH_LAKE).build().maxAreaOfIsland());
+    assertEquals(10, Islands.builder().grid(TWO_ISLANDS_ONE_WITH_LAKE).build().maxAreaOfIsland());
   }
 
   @Test
@@ -26,6 +32,25 @@ public class IslandsTest {
     assertEquals(2, Islands.builder().grid(ONE_ISLAND_WITH_LAKE).build().countOceanOrLakes());
     assertEquals(1, Islands.builder().grid(ISLAND_WITH_BAY).build().countOceanOrLakes());
     assertEquals(2, Islands.builder().grid(TWO_ISLANDS_ONE_WITH_LAKE).build().countOceanOrLakes());
+  }
+
+  @Test
+  public void maxAreaOfContinentWithNoWaterInBorders() {
+    // We clone this array to avoid modifyin the ref
+    int[][] input = Arrays.stream(TWO_ISLANDS_ONE_WITH_LAKE).map(int[]::clone).toArray(int[][]::new);
+    Islands map = Islands.builder().grid(input).build();
+    map.fillLakesWithLand();
+    assertEquals(11, map.maxAreaOfIsland());
+    assertEquals(1, map.countOceanOrLakes());
+  }
+
+  @Test
+  @Ignore
+  public void maxAreaOfContinentWaterInBorders() {
+    Islands map = Islands.builder().grid(ONE_ISLANDS_ONE_WITH_LAKE).build();
+    map.fillLakesWithLand();
+    assertEquals(11, map.maxAreaOfIsland());
+    assertEquals(1, map.countOceanOrLakes());
   }
 
 }
